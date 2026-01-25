39c0959fb147706b13f88ee34497dc2ac27a5f6f
julien <juliengenovese@hotmail.it>
2026-01-25 12:53:15 +0000

    refactor: refactor of extractor for better clarity

--- src/data_extractor/extractor.py
+++ src/data_extractor/extractor.py
@@ -7,147 +7,112 @@ from google import genai
 from google.genai import types
 from loguru import logger
 
-from src.model import OffertaEnergia
+from src.model import Offerta
 from ..config import config  
 
-CACHE_DIR = "cache"
-CACHE_TTL_SECONDS = 24 * 3600  
 
-
-class EnergyGeminiExtractor:
-    """
-    Classe per estrarre dati strutturati da PDF usando Google Gemini.
-    """
-
-    def __init__(self, api_key=None, prompt_file=None, model="gemini-2.5-flash"):
-        self.api_key = api_key or config.get("GENAI_API_KEY")
-        self.prompt_file = prompt_file or config.get("PROMPT_LUCE_FILE")
-        self.model = model or config.get("GENAI_MODEL", "gemini-2.5-flash")
-
-        if not self.api_key:
-            raise ValueError("GENAI_API_KEY non trovato. Controlla il file .env.")
-        if not self.prompt_file or not os.path.exists(self.prompt_file):
-            raise FileNotFoundError(f"Prompt file non trovato: {self.prompt_file}")
-
-        self.client = genai.Client(api_key=self.api_key)
-
-        self._load_prompt()
-        
-    def _cache_key(self, pdf_path: str) -> str:
-        """Genera una chiave di cache unica basata sul file PDF e il prompt
-        utilizzato.
-        Args:
-            pdf_path (str): Percorso del file PDF.
-        Returns:
-            str: Chiave di cache unica.
-        """
-        stat = os.stat(pdf_path)
-
-        payload = f"""
-        {os.path.basename(pdf_path)}
-        {stat.st_size}
-        {self.model}
-        {self.prompt_text}
-        """
-
-        return hashlib.sha256(payload.encode("utf-8")).hexdigest()
+class CacheManager:
+    def __init__(self, cache_dir: str, ttl_seconds: int):
+        self.cache_dir = cache_dir
+        self.ttl_seconds = ttl_seconds
+        os.makedirs(cache_dir, exist_ok=True)
 
     def _cache_path(self, key: str) -> str:
-        """Restituisce il percorso del file di cache per una chiave data."""
-        return os.path.join(CACHE_DIR, f"{key}.json")
+        return os.path.join(self.cache_dir, f"{key}.json")
 
-    def _load_from_cache(self, key: str):
-        """Carica i dati dalla cache se esistono e non sono scaduti."""
+    def save(self, key: str, data: dict):
+        with open(self._cache_path(key), "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False)
+
+    def load(self, key: str):
         path = self._cache_path(key)
         if not os.path.exists(path):
             return None
-
-        if time.time() - os.path.getmtime(path) > CACHE_TTL_SECONDS:
+        if time.time() - os.path.getmtime(path) > self.ttl_seconds:
             return None
-
-        logger.info("Carichiamo da cache...")
         with open(path, "r", encoding="utf-8") as f:
             return json.load(f)
+
+    def generate_key(self, *args) -> str:
+        payload = "\n".join(str(a) for a in args)
+        return hashlib.sha256(payload.encode("utf-8")).hexdigest()
+
+
+class DebugProvider:
+    def get_offerta(self, pdf_path: str):
+        return {
+            "nome_offerta": "NEXTENERGYSMARTLUCE",
+            "gestore": "Sorgenia",
+            "prezzo_stimato_offerta_kwh": 0.14,
+            "prezzo_stimato_finita_kwh": None,
+            "durata_mesi": 12,
+            "costi_fissi_anno": 81.03,
+            "tipologia_formula_offerta": "costante",
+            "tipologia_formula_finita": "standard",
+            "fee_offerta_kwh": 0.008,
+            "fee_finita_kwh": 0.03,
+            "note": "Uscita non possibile prima di 24 mesi"
+        }
         
-    def _save_to_cache(self, key: str, data: dict):
-        """Salva i dati nella cache."""
-        self._ensure_cache_dir()
-        path = self._cache_path(key)
-        with open(path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False)
+class EnergyGeminiExtractor:
+    def __init__(self, model="gemini-2.5-flash", prompt_text=""):
+        self.api_key = config.get("GENAI_API_KEY")
+        self.model = model
+        self.prompt_text = prompt_text
+        self.cache = CacheManager(config.get("CACHE_DIR"), float(config.get("CACHE_TTL_SECONDS")))
 
-    def _ensure_cache_dir(self):
-        """Crea la cartella di cache se non esiste."""
-        os.makedirs(CACHE_DIR, exist_ok=True)
+        if not self.api_key:
+            raise ValueError("GENAI_API_KEY non trovato. Controlla il file 'keys.env'")
+        self.client = genai.Client(api_key=self.api_key)
 
-    def _load_prompt(self):
-        """Carica il testo del prompt in memoria."""
-        with open(self.prompt_file, "r", encoding="utf-8") as f:
-            self.prompt_text = f.read()
+    def extract(self, pdf_path: str, use_cache: bool = True) -> Offerta:
+        logger.info(f"[{pdf_path}] Inizio estrazione dati con Energy Gemini")
+        cache_key = self.cache.generate_key(pdf_path, self.model, self.prompt_text)
 
-    def _clean_text(self, raw_text):
-        """Rimuove i blocchi di codice Markdown e spazi inutili."""
-        cleaned = re.sub(r"```.*?\n", "", raw_text).replace("```", "").strip()
-        return cleaned
+        if use_cache:
+            cached_data = self.cache.load(cache_key)
+            if cached_data:
+                logger.success(f"[{pdf_path}] Dati caricati dalla cache.")
+                return Offerta(**cached_data)
+            logger.info(f"[{pdf_path}] Nessun dato in cache.")
 
-    def extract_from_pdf(self, path_to_pdf, is_debug: bool =False, use_cache: bool = True) -> dict:
-        """Estrae dati strutturati da un PDF."""
-        
-        if is_debug:
-            logger.debug(f"Estrazione da PDF: {path_to_pdf}")
-            dict_result ={
-                "nome_offerta": "NEXTENERGYSMARTLUCE",
-                "gestore": "Sorgenia",
-                "prezzo_stimato_offerta_kwh": 0.14,
-                "prezzo_stimato_finita_kwh": None,
-                "durata_mesi": 12,
-                "costi_fissi_anno": 81.03,
-                "tipologia_formula_offerta": "costante",
-                "tipologia_formula_finita": "standard",
-                "fee_offerta_kwh": 0.008,
-                "fee_finita_kwh": 0.03,
-                "note": "Uscita non possibile prima di 24 mesi"
+        uploaded_file = self.client.files.upload(file=pdf_path)
+        response_text = None
+        try:
+            parts = [
+                types.Part.from_text(text=self.prompt_text),
+                types.Part.from_uri(file_uri=uploaded_file.uri, mime_type=uploaded_file.mime_type)
+            ]
+            contents = [types.Content(role="user", parts=parts)]
+
+            response = self.client.models.generate_content(
+                model=self.model,
+                contents=contents,
+                config={
+                    "response_mime_type": "application/json",
+                    "response_json_schema": Offerta.model_json_schema()  
                 }
-            return OffertaEnergia(**dict_result)
-        else:
-                
-            if not os.path.exists(path_to_pdf):
-                raise FileNotFoundError(f"File PDF non trovato: {path_to_pdf}")
-            cache_key = self._cache_key(path_to_pdf)
+            )
+            response_text = response.text
 
-            if use_cache:
-                cached = self._load_from_cache(cache_key)
-                if cached is not None:
-                    logger.success("Dati caricati dalla cache.")
-                    return OffertaEnergia(**cached)
-                
-            uploaded_file = self.client.files.upload(file=path_to_pdf)
-            logger.success(f"File {path_to_pdf} caricato.")
+            result_dict = json.loads(self._clean_text(response_text))
+            offerta = Offerta(**result_dict)
 
-            try:
-                parts = [
-                    types.Part.from_text(text=self.prompt_text),
-                    types.Part.from_uri(
-                        file_uri=uploaded_file.uri,
-                        mime_type=uploaded_file.mime_type
-                    ),
-                ]
-                contents = [types.Content(role="user", parts=parts)]
+            self.cache.save(cache_key, result_dict)
+            logger.success(f"[{pdf_path}] Estrazione completata e salvata in cache.")
+            return offerta
 
-                response = self.client.models.generate_content(
-                    model=self.model,
-                    contents=contents
-                )
+        finally:
+            if response_text:
+                with open("last_response.txt", "w", encoding="utf-8") as f:
+                    f.write(response_text)
+            self.client.files.delete(name=uploaded_file.name)
 
-                cleaned_text = self._clean_text(response.text)
-                result = json.loads(cleaned_text)
+    @staticmethod
+    def _clean_text(raw_text: str) -> str:
+        """Rimuove eventuali blocchi di Markdown``` e spazi inutili."""
+        return raw_text.replace("```", "").strip()
 
-                self._save_to_cache(cache_key, result)
-                logger.success("Estrazione completata e salvata in cache.")
-                return OffertaEnergia(**result)
-
-            finally:
-                self.client.files.delete(name=uploaded_file.name)
 
 
 if __name__ == "__main__":
